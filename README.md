54f5ff688f97d739d64474d26b573d628adf803f
ch663p <chris.hutchings@intl.att.com>
2020-11-16 17:34:52 +1300

    Add alert limit of 0 on vulnerabilities

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # shodanprtg
 PRTG sensor that monitors Shodan API for open ports and vulnerabilities
+
+For devices being monitored in PRTG that have a public IP address, you can track the number of open ports and vulnerabilities that are detected by Shodan.
+
+Paste the shodan.py file into the custom sensors directory on the PRTG machine that will be doing the query. If you have remote probes, it will have to be on those machines. The custom sensor directory is found here: C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\python
+
+Add this as a "python script advanced" sensor and pick the shodan.py file from the drop down. Put your shodan API key in the Additional Parameters field.
+
+You should then see the results show up in PRTG. You should set appropriate warning and error limits on the channels that you want to be alerted about.
--- shodan.py
+++ shodan.py
@@ -19,14 +19,14 @@ try:
     sensor = CustomSensorResult(msg)
     sensor.add_channel('Open Ports',value=len(hostData['ports']),unit='ports')
     if 'vulns' in hostData.keys():
-        sensor.add_channel('Vulnerabilities',value=len(hostData['vulns']),unit='vulns')
+        sensor.add_channel('Vulnerabilities',value=len(hostData['vulns']),unit='vulns',is_limit_mode=True,limit_max_error=0)
     else:
-        sensor.add_channel('Vulnerabilities',value=0,unit='vulns')
+        sensor.add_channel('Vulnerabilities',value=0,unit='vulns',is_limit_mode=True,limit_max_error=0)
 except urllib.error.HTTPError as err:
     if err.code == 404:
         sensor = CustomSensorResult('No Data')
         sensor.add_channel('Open Ports',value=0,unit='ports')
-        sensor.add_channel('Vulnerabilities',value=0,unit='vulns')
+        sensor.add_channel('Vulnerabilities',value=0,unit='vulns',is_limit_mode=True,limit_max_error=0)
 except Exception as err:
     sensor = CustomSensorResult()
     sensor.add_error("Error - {0}".format(repr(err)))
